be39fd5089348af58685453bda09de6b7d94ef2d
metamethods <tabledmetamethods@gmail.com>
2026-01-01 16:45:57 -0800

    refactor: let BotError impl<E: Display> From<E>

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use std::borrow::Cow;
+use std::fmt::Display;
 
 use twilight_model::http::interaction::InteractionResponseData;
 
@@ -9,86 +9,8 @@ pub enum BotError {
     String(String),
 }
 
-impl From<String> for BotError {
-    fn from(value: String) -> Self {
-        BotError::String(value)
-    }
-}
-
-impl From<&str> for BotError {
-    fn from(value: &str) -> Self {
-        BotError::String(value.into())
-    }
-}
-
-impl<'a> From<Cow<'a, str>> for BotError {
-    fn from(value: Cow<'a, str>) -> Self {
-        BotError::String(value.into())
-    }
-}
-
-impl From<std::io::Error> for BotError {
-    fn from(value: std::io::Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<reqwest::Error> for BotError {
-    fn from(value: reqwest::Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<twilight_http::Error> for BotError {
-    fn from(value: twilight_http::Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<ril::Error> for BotError {
-    fn from(value: ril::Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<dotenvy::Error> for BotError {
-    fn from(value: dotenvy::Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<std::env::VarError> for BotError {
-    fn from(value: std::env::VarError) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<twilight_http::response::DeserializeBodyError> for BotError {
-    fn from(value: twilight_http::response::DeserializeBodyError) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<twilight_gateway::error::StartRecommendedError> for BotError {
-    fn from(value: twilight_gateway::error::StartRecommendedError) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<shell_words::ParseError> for BotError {
-    fn from(value: shell_words::ParseError) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<std::string::FromUtf8Error> for BotError {
-    fn from(value: std::string::FromUtf8Error) -> Self {
-        BotError::String(format!("{value:#?}"))
-    }
-}
-
-impl From<chrono::ParseError> for BotError {
-    fn from(value: chrono::ParseError) -> Self {
-        BotError::String(format!("{value:#?}"))
+impl<E: Display> From<E> for BotError {
+    fn from(value: E) -> Self {
+        BotError::String(value.to_string())
     }
 }
